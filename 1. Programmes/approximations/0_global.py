95fb3a3229a9722a4ec567c273b3c5952887e603
Quentin <45366083+quentinprieels@users.noreply.github.com>
2021-04-01 10:04:10 +0200

    Plot functions to use with simulations

--- 1. Programmes/approximations/0_global.py
+++ 1. Programmes/approximations/0_global.py
@@ -1,5 +1,19 @@
-from numpy import *
 import matplotlib.pyplot as plt
 
- # Coucou github
- #hello 
+
+def plot_one(x, signal, signalname="$$", xname="$$", yname="$$"):
+    plt.plot(x, signal, label=signalname)
+    plt.xlabel = xname
+    plt.ylabel = yname
+    plt.legend()
+    plt.show()
+
+
+def plot_two(x, signal_1, signal_2, signal_1_name="$$", signal_2_name="$$", xname="$$", yname="$$"):
+    plt.plot(x, signal_1, label=signal_1_name)
+    plot_one(x, signal_2, signal_2_name, xname, yname)
+
+
+def plot_three(x, signal_1, signal_2, signal_3, signal_1_name="$$", signal_2_name="$$", signal_3_name="$$", xname="$$", yname="$$"):
+    plt.plot(x, signal_1, label=signal_1_name)
+    plot_two(x, signal_2, signal_3, signal_2_name, signal_3_name, xname, yname)
