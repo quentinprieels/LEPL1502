c1df62e49ebcf57ee2b9d622cbe57f0cc81898ff
timdefranne <81739991+timdefranne@users.noreply.github.com>
2021-04-28 14:19:14 +0200

    Update 2_amplificateur.py

--- 1. Programmes/approximations/2_amplificateur.py
+++ 1. Programmes/approximations/2_amplificateur.py
@@ -1,1 +1,27 @@
-from numpy import *
+import numpy as np
+from matplotlib import pyplot as plt 
+
+def vl(t):
+    vl = np.sin(t)
+    return vl 
+
+def vamp(vl,pramp,ramp):
+    for i in range(len(vl)):
+        if vl[i]<= 0 :
+            vl[i]=0
+    v = vl*(1+(pramp/ramp))
+    return v    
+
+ramp = 5600
+t = np.linspace(-10,10,1000)
+Vl = 0.4 * vl(t)
+pramp = 50400
+
+y = vamp(Vl,pramp,ramp) 
+Vamp = plt.plot(t,y,'r', label ='V_amp')
+sin = plt.plot(t,0.4*vl(t),'b', label = 'V_L')
+plt.ylim(-1,5)
+plt.xlabel("Temps (ms)")
+plt.ylabel("Tension (V)")
+plt.legend(loc = 'upper right')
+plt.show()
